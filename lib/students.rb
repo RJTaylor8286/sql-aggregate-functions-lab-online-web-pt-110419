da21c0fb96ce5a130746608841a14ffe9f952c27
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2020-02-06 16:10:22 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,10 +5,10 @@ CREATE TABLE students (
   tardies INTEGER
 );
 
-INSERT INTO students (name,highest_student_gpa) VALUES ('Zak', 4);
+INSERT INTO students (name, gpa) VALUES ('Zak', 4);
 end
 
-INSERT INTO students (name, lowest_student_gpa) VALUES ('Bob', 1.9);
+INSERT INTO students (name, gpa) VALUES ('Bob', 1.9);
 end
 
 def average_student_gpa
